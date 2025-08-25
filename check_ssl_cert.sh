9c20990b3212208d3799962184328fe627f949b4
Ihor <148973465+iamihor@users.noreply.github.com>
2025-08-25 18:48:46 +0300

    Create check_ssl_cert.sh

---
+++ check_ssl_cert.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+DEBUG=0
+if [ $DEBUG -gt 0 ]
+then
+    exec 2>>/tmp/my.log
+    set -x
+fi
+
+if [ $# -lt 1 ]
+then
+    echo ""
+    echo "Usage: $0 hostname"
+    echo ""
+    exit 0
+fi
+
+servername=$1
+
+end_date="$(curl -vI https://$servername 2>&1 | sed -n 's/.*expire date: //p')"
+
+if [ -n "$end_date" ]
+then
+    end_date_seconds=`date '+%s' --date "$end_date"`
+    now_seconds=`date '+%s'`
+    echo "($end_date_seconds-$now_seconds)/24/3600" | bc
+fi
