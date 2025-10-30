2fb624a64bbad8a3c73384b8e4a719480b1b0dbe
Ihor <148973465+iamihor@users.noreply.github.com>
2025-10-30 13:17:28 +0200

    Update check_ssl_cert.sh
    Add check if busybox (alpine linux for docker) version of date.

--- check_ssl_cert.sh
+++ check_ssl_cert.sh
@@ -1,17 +1,15 @@
-#! /bin/sh
+#! /bin/bash
 
 DEBUG=0
-if [ $DEBUG -gt 0 ]
-then
+if [ $DEBUG -gt 0 ]; then
     exec 2>>/tmp/my.log
     set -x
 fi
 
-if [ $# -lt 1 ]
-then
+if [ $# -lt 1 ]; then
     echo ""
     echo "Usage: $0 hostname"
-    echo ""
+    echo "-1"
     exit 0
 fi
 
@@ -19,9 +17,19 @@ servername=$1
 
 end_date="$(curl -vkI https://$servername 2>&1 | sed -n 's/.*expire date: //p')"
 
-if [ -n "$end_date" ]
-then
-    end_date_seconds=`date '+%s' --date "$end_date"`
+BUSYBOX=0
+if date --version 2>&1 | grep -qi 'busybox' ; then
+    BUSYBOX=1
+fi
+
+if [ -n "$end_date" ]; then
+    if [ $BUSYBOX -eq 1 ]; then
+        end_date_seconds=`date '+%s' --date "$end_date" -D '%b %d %H:%M:%S %Y'`
+    else
+        end_date_seconds=`date '+%s' --date "$end_date"`
+    fi
     now_seconds=`date '+%s'`
     echo "($end_date_seconds-$now_seconds)/24/3600" | bc
+else
+    echo -1
 fi
